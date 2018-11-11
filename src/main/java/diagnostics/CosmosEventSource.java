6f18660941c71eebbe02fa30a4562fb0c117ac42
Theodoros Zarkopafilis Ntakouris <zarkopafilis@gmail.com>
2018-11-11 19:11:40 +0200

    Remove CosmosPagedResults & CosmosEventSource

--- src/main/java/diagnostics/CosmosEventSource.java
+++
@@ -1,4 +0,0 @@
-package diagnostics;
-
-public class CosmosEventSource {
-}
--- src/main/java/response/CosmosMultipleResponse.java
+++ src/main/java/response/CosmosMultipleResponse.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CosmosMultipleResponse<TEntity> {
-
-
     List<CosmosResponse<TEntity>> failedEntities = new ArrayList<>();
 
     List<CosmosResponse<TEntity>> successfulEntities = new ArrayList<>();
